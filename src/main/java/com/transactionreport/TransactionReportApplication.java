0020835037372665ab785fe9355ced835468bd97
Abudzar Alghiffari <abudzar.alghiffari@ifg-life.id>
2024-12-12 18:52:18 +0700

    initial commit

---
+++ src/main/java/com/transactionreport/TransactionReportApplication.java
@@ -0,0 +1,13 @@
+package com.transactionreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionReportApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(TransactionReportApplication.class, args);
+  }
+  
+}
